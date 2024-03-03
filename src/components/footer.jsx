5cf609b1b1627b6c22e2c68373673eb385ee3302
Constanza Cabrera <constanza.cabrerao@gmail.com>
2024-03-03 12:00:59 -0800

    commit 3 del desafio2

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,26 @@
 const Footer = () => {
-    return (
-      <>
-        <footer>
-       <p>
-            Explora nuestra galería de adopción de perros para encontrar a tu
-            compañero perfecto. Tenemos una variedad de perros con diferentes
-            personalidades y tamaños, todos en busca de hogar amoroso. Cada imagen
-            captura su esencia única. Adopta un perro y bríndale una segunda
-            oportunidad. Encuentra a tu compañero peludo para siempre.
-          </p>
-          <div className="footer-icon">
-            <a href="#"><i className="fa-brands fa-facebook"></i></a>
-            <a href="#"><i className="fa-brands fa-instagram"></i></a>
-            <a href="#"><i className="fa-brands fa-whatsapp"></i></a>
-          </div>
-          <p>
-            ©2024 Todos los derechos reservados - Adopta Perrito
-          </p>
-        </footer>
-      </>
-    );
-  };
-  
-  export default Footer;
+  return (
+    <>
+      <footer>
+        <p>
+          PERDON PERO DEBO HACER CAMBIOS PARA EL DESAFIO 2ro peludo para
+          siempre.
+        </p>
+        <div className="footer-icon">
+          <a href="#">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-whatsapp"></i>
+          </a>
+        </div>
+        <p>©2024 Todos los derechos reservados - Adopta Perrito</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
